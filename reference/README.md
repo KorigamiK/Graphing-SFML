8f3d26478a8098a5f687aa21a688871a22b8fee4
korigamik <korigamik@gmail.com>
2022-07-19 23:47:27 +0530

    add working reference

---
+++ reference/README.md
@@ -0,0 +1,24 @@
+# Conics
+
+    A cleaned up version of CodeParade's conic solver with build configuration for vscode on linux.
+    
+An interactive conic visualization app used to record [**This Video**](https://youtu.be/X83vac2uTUs).
+
+[**Download (Windows 64bit)**](https://github.com/HackerPoet/Conics/raw/master/Conics.zip)
+
+## Key Bindings
+* Space Bar - Advance to the next animation
+* Mouse - Click and drag on points to interact (not all points are movable)
+* C - Capture a screenshot and save it to the local directory.
+* Escape - Exit the app.
+
+## Building
+
+Dependencies: `SFML 2`, `Eigen 3`.
+
+Building with CMake:
+
+* `mkdir build`
+* `cd build`
+* `cmake ..`
+* `make`
---
+++ reference/include/Ellipse.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <Eigen/Dense>
+#include <cmath>
+
+class Ellipse
+{
+public:
+  typedef Eigen::Vector2d Vector2d;
+  static const double pi;
+  static long double CrossMag(const Vector2d &a, const Vector2d &b)
+  {
+    return a.x() * b.y() - a.y() * b.x();
+  }
+  static long double CrossMagSqr(const Vector2d &a, const Vector2d &b)
+  {
+    const double mag = CrossMag(a, b);
+    return mag * mag;
+  }
+
+  // Constructor
+  Ellipse(const Vector2d &a = Vector2d::UnitX(),
+          const Vector2d &b = Vector2d::UnitY(),
+          const Vector2d &c = Vector2d::Zero()) : v_a(a), v_b(b), v_c(c) {}
+
+  // Constructor from [ax^2 + 2bxy + cy^2 + 2dx + 2ey + f = 0]
+  bool FromConic(double a, double b, double c, double d, double e, double f);
+  void ToConic(double &a, double &b, double &c, double &d, double &e, double &f, bool hyperbola = false) const;
+
+  // Rotational
+  Vector2d VectorAtAngle(long double theta) const
+  {
+    return v_a * std::cos(theta) + v_b * std::sin(theta);
+  }
+  Vector2d PointAtAngle(long double theta) const
+  {
+    return v_a * std::cos(theta) + v_b * std::sin(theta) + v_c;
+  }
+  Vector2d PointAtAngleH1(long double theta) const
+  {
+    return v_a * std::cosh(theta) + v_b * std::sinh(theta) + v_c;
+  }
+  Vector2d PointAtAngleH2(long double theta) const
+  {
+    return -v_a * std::cosh(theta) + v_b * std::sinh(theta) + v_c;
+  }
+  Vector2d VelocityAtAngle(long double theta) const
+  {
+    return v_a * -std::sin(theta) + v_b * std::cos(theta);
+  }
+
+  // Others
+  bool IsInside(const Vector2d &pt) const
+  {
+    return CrossMagSqr(pt - v_c, v_a) + CrossMagSqr(pt - v_c, v_b) < CrossMagSqr(v_a, v_b);
+  }
+
+  // Invariants
+  double CSqr() const
+  {
+    return v_a.squaredNorm() + v_b.squaredNorm();
+  }
+  double Area() const
+  {
+    return pi * CrossMag(v_a, v_b);
+  }
+
+  Vector2d v_a;
+  Vector2d v_b;
+  Vector2d v_c;
+};
